33e2201c4f1692a3f980154dbd0921901873c318
Luiz R. Lencioni <luiz.lencioni@gmail.com>
2023-12-02 21:27:13 +0100

    Create single-responsibility-principle.cpp

---
+++ single-responsibility-principle.cpp
@@ -0,0 +1,35 @@
+/***************************************************
+*
+* This example shows the correct way of implementing the Single Responsability Principle of SOLID
+*
+* (One way of do it would be join all methods into a single class User ... not a good programming principle!)
+*
+****************************************************/
+
+class Authentication {
+public:
+    void login() {
+        cout << "User logged in" << endl;
+    }
+    void logout() {
+        cout << "User logged out" << endl;
+    }
+};
+
+class UserInfo {
+public:
+    void printUserInfo(string username) {
+        cout << "Username: " << username << endl;
+    }
+};
+
+class User : public Authentication, public UserInfo {
+};
+
+int main() {
+    User user;
+    user.login();
+    user.printUserInfo("Abhishek");
+    user.logout();
+    return 0;
+}{
